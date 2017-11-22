0a23fa0ba32acbba61eca66f9b405e4c8b0ee8e3
Ghouli <huhtala.teemu@gmail.com>
2017-11-22 20:07:59 +0200

    Added support for e-mail attachments

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule BambooSmtp.Mixfile do
 
   defp deps do
     [
-      {:bamboo, "~> 1.0.0-rc"},
+      {:bamboo, "~> 1.0.0-rc.2"},
       {:credo, "~> 0.8.2", only: [:dev, :test]},
       {:earmark, ">= 1.0.3", only: :dev},
       {:ex_doc, "~> 0.16.2", only: :dev},
