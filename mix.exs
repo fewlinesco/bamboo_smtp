ecce0c59ba426e5f63e9b361851ee24e45ff7617
Julien Leclercq <julien.leclercq@gmx.fr>
2019-05-23 14:04:23 +0200

    update credo

--- mix.exs
+++ mix.exs
@@ -4,19 +4,21 @@ defmodule BambooSmtp.Mixfile do
   @project_url "https://github.com/fewlinesco/bamboo_smtp"
 
   def project do
-    [app: :bamboo_smtp,
-     version: "1.6.0",
-     elixir: "~> 1.4",
-     source_url: @project_url,
-     homepage_url: @project_url,
-     name: "Bamboo SMTP Adapter",
-     description: "A Bamboo adapter for SMTP",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     package: package(),
-     deps: deps(),
-     docs: [main: "readme", extras: ["README.md"]]]
+    [
+      app: :bamboo_smtp,
+      version: "1.6.0",
+      elixir: "~> 1.4",
+      source_url: @project_url,
+      homepage_url: @project_url,
+      name: "Bamboo SMTP Adapter",
+      description: "A Bamboo adapter for SMTP",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      package: package(),
+      deps: deps(),
+      docs: [main: "readme", extras: ["README.md"]]
+    ]
   end
 
   def application do
@@ -26,7 +28,7 @@ defmodule BambooSmtp.Mixfile do
   defp deps do
     [
       {:bamboo, "~> 1.0"},
-      {:credo, "~> 0.8.10", only: [:dev, :test]},
+      {:credo, "~> 1.0.5", only: [:dev, :test]},
       {:earmark, ">= 1.0.3", only: :dev},
       {:ex_doc, "~> 0.16.2", only: :dev},
       {:excoveralls, "~> 0.7.1", only: :test},
