85e8de7895d20d5a17a22bb7879be2eb11d86881
Julien Leclercq <julien.leclercq@gmx.fr>
2019-05-23 17:51:07 +0200

    version minor bump

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule BambooSmtp.Mixfile do
   def project do
     [
       app: :bamboo_smtp,
-      version: "1.6.0",
+      version: "1.7.0",
       elixir: "~> 1.4",
       source_url: @project_url,
       homepage_url: @project_url,
@@ -29,15 +29,15 @@ defmodule BambooSmtp.Mixfile do
     [
       {:bamboo, "~> 1.2"},
       {:credo, "~> 1.0.5", only: [:dev, :test]},
-      {:gen_smtp, "~> 0.14.0"},
       {:earmark, ">= 1.3.2", only: :docs},
       {:excoveralls, "~> 0.11.1", only: :test},
       {:ex_doc, ex_doc_version(), only: :docs},
+      {:gen_smtp, "~> 0.14.0"},
       {:inch_ex, "~> 2.0.0", only: :docs}
     ]
   end
 
-  def ex_doc_version do
+  defp ex_doc_version do
     if Version.match?(System.version(), "~> 1.7") do
       "~> 0.20.2"
     else
