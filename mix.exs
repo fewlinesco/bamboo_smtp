c82af32afa95fc5167163deb58c3b4d19bc3ebcd
julien-leclercq <julien@fewlines.co>
2019-05-27 10:53:21 +0200

    update dependencies (#115)
    ## Description 
    
    This PR contains :
    - an update for `gen_smtp` that allows us to support `otp 22` 
    - addition of newer elixir and otp version in travis build configuration 
    - a minor bump
    
    resolve #114

--- mix.exs
+++ mix.exs
@@ -4,19 +4,21 @@ defmodule BambooSmtp.Mixfile do
   @project_url "https://github.com/fewlinesco/bamboo_smtp"
 
   def project do
-    [app: :bamboo_smtp,
-     version: "1.6.0",
-     elixir: "~> 1.4",
-     source_url: @project_url,
-     homepage_url: @project_url,
-     name: "Bamboo SMTP Adapter",
-     description: "A Bamboo adapter for SMTP",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     package: package(),
-     deps: deps(),
-     docs: [main: "readme", extras: ["README.md"]]]
+    [
+      app: :bamboo_smtp,
+      version: "1.7.0",
+      elixir: "~> 1.4",
+      source_url: @project_url,
+      homepage_url: @project_url,
+      name: "Bamboo SMTP Adapter",
+      description: "A Bamboo adapter for SMTP",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      package: package(),
+      deps: deps(),
+      docs: [main: "readme", extras: ["README.md"]]
+    ]
   end
 
   def application do
@@ -25,14 +27,22 @@ defmodule BambooSmtp.Mixfile do
 
   defp deps do
     [
-      {:bamboo, "~> 1.0"},
-      {:credo, "~> 0.8.10", only: [:dev, :test]},
-      {:earmark, ">= 1.0.3", only: :dev},
-      {:ex_doc, "~> 0.16.2", only: :dev},
-      {:excoveralls, "~> 0.7.1", only: :test},
-      {:gen_smtp, "~> 0.12.0"},
-      {:inch_ex, "~> 0.5.5", only: :docs}
+      {:bamboo, "~> 1.2"},
+      {:credo, "~> 1.0.5", only: [:dev, :test]},
+      {:earmark, ">= 1.3.2", only: :docs},
+      {:excoveralls, "~> 0.11.1", only: :test},
+      {:ex_doc, ex_doc_version(), only: :docs},
+      {:gen_smtp, "~> 0.14.0"},
+      {:inch_ex, "~> 2.0.0", only: :docs}
     ]
+  end
+
+  defp ex_doc_version do
+    if Version.match?(System.version(), "~> 1.7") do
+      "~> 0.20.2"
+    else
+      "~> 0.18.4"
+    end
   end
 
   defp package do
