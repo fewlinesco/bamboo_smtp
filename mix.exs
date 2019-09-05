2b67091ee3b52002de2f11d54e98204d8e3310d7
Julien Leclercq <julien.leclercq@gmx.fr>
2019-09-05 12:09:17 +0200

    bump credo

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule BambooSmtp.Mixfile do
   defp deps do
     [
       {:bamboo, "~> 1.2"},
-      {:credo, "~> 1.0.5", only: [:dev, :test]},
+      {:credo, "~> 1.1.4", only: [:dev, :test]},
       {:earmark, ">= 1.3.2", only: :docs},
       {:excoveralls, "~> 0.11.1", only: :test},
       {:ex_doc, ex_doc_version(), only: :docs},
