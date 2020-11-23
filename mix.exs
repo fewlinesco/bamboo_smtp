fdee3cc11306e086ba8ca64bc7120b9da72d4784
destne <matheus.el.eng@gmail.com>
2020-11-23 11:56:07 +0100

    change version and changelog

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule BambooSmtp.Mixfile do
   def project do
     [
       app: :bamboo_smtp,
-      version: "3.0.0",
+      version: "3.1.0",
       elixir: "~> 1.7",
       source_url: @project_url,
       homepage_url: @project_url,
