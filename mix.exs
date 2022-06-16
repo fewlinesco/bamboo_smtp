fc70c25267d07e5ac8284f224d3b1eb4c010eee3
Jacob Chae <jacob.chae@industrialml.com>
2022-06-16 09:04:31 +0900

    Remove explicit ranch dependency
    We originally added this to help address issues with older Elixir / OTP
    versions, but we ended up dropping support for those so hopefully
    removing this will fix the conflicts with the latest `cowboy` version
    (2.9.0).

--- mix.exs
+++ mix.exs
@@ -31,7 +31,6 @@ defmodule BambooSmtp.Mixfile do
       # core
       {:bamboo, "~> 2.2.0"},
       {:gen_smtp, "~> 1.2.0"},
-      {:ranch, "2.0.0"},
 
       # dev / test
       {:credo, "~> 1.6.1", only: [:dev, :test]},
